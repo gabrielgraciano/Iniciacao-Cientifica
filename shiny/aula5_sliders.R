cbbd26e5c8448cb395da3ec1cf2bf53dd136d0d3
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2023-01-11 21:51:39 -0300

    Update and rename aula5.R to aula5_sliders.R

--- shiny/aula5_sliders.R
+++ shiny/aula5_sliders.R
@@ -1,29 +1,29 @@
 library(shiny)
 
 ui <- fluidPage(
-titlePanel("Demonstration of sliderInput widget in shiny"),
+  titlePanel("Demonstração de slide bar"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("slide", "select the value from slider", min = 0, max = 5, value = 2, 
-                  animate = T, step=0.2)
-      
-      
+      sliderInput("Desliza", "Select the value from slider", min = 0, max = 20,
+                  value = c(1, 2), animate = T, step = 0.5)
     ),
     mainPanel(
       
       textOutput("out")
     )
-    
+  
   )
-    
 )
-
+    
 server <- function(input, output) {
+  
   output$out <- renderText(
-    paste("You selected the value as: ", input$slide))
+    paste("A distância entre os valores selecionados é", input$Desliza[2] - input$Desliza[1]))
 }
 
+
 shinyApp(ui, server)
+
 
 #funções importantes: sliderInput(animate = ): anima a barra de slider.
 #Slider está dentro de sidebarPanel. sliderInput(step = ) permite ampliar ou reduzir 
