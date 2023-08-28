50662cc2947b9cb1f1276359c44e2e7d560fb987
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2023-08-28 07:56:58 -0300

    Create ex7.R

---
+++ shiny/mastering shiny book/chapter_2/ex7.R
@@ -0,0 +1,15 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  dataTableOutput('table')
+)
+
+server <- function(input, output, session) {
+  output$table <- renderDataTable(mtcars, options = list(pageLength = 5,
+                                                         ordering = FALSE,
+                                                         searching = FALSE))
+}
+
+
+shinyApp(ui = ui, server = server)
